65b32213abbab8cc5ae48abcfefd964cc204297d
Crowdbotics <team@crowdbotics.com>
2021-01-25 04:55:38 +0000

    Update models

--- backend/on_call_24038/wsgi.py
+++ backend/on_call_24038/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'on_call_24038.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "on_call_24038.settings")
 
 application = get_wsgi_application()
